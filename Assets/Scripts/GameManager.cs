98a6269e723a38f61ad653b0d5bf6216f1c9f0bd
César <cesasanc@student.hive.fi>
2026-04-27 18:30:54 +0300

    chore(game): fix pause and restart flow

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -15,30 +17,67 @@ public class GameManager : MonoBehaviour
 	public void GameOver()
 	{
 		IsGameOver = true;
+		ResumeGame();
 	}
 
 	public void PauseGame()
 	{
+		if (IsGamePaused)
+		{
+			return;
+		}
+
 		IsGamePaused = true;
-		Time.timeScale = IsGamePaused ? 0 : 1;
+		Time.timeScale = 0f;
+
+		UIManager uiManager = UIManager.Instance;
+		if (uiManager != null)
+		{
+			uiManager.ShowPausePanel(true);
+		}
 	}
 
 	public void ResumeGame()
 	{
+		if (!IsGamePaused)
+		{
+			return;
+		}
+
+		IsGamePaused = false;
 		Time.timeScale = 1f;
-		UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+
+		UIManager uiManager = UIManager.Instance;
+		if (uiManager != null)
+		{
+			uiManager.ShowPausePanel(false);
+		}
+	}
+
+	public void RestartLevel()
+	{
+		Time.timeScale = 1f;
+		IsGamePaused = false;
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 	}
 
 	void Update()
 	{
-		if (IsGameOver && Input.GetKeyDown(KeyCode.R))
+		if (Keyboard.current != null && IsGameOver && Keyboard.current.rKey.wasPressedThisFrame)
 		{
-			ResumeGame();
+			RestartLevel();
 		}
 
-		if (Input.GetKeyDown(KeyCode.Escape))
+		if (Keyboard.current != null && !IsGameOver && Keyboard.current.escapeKey.wasPressedThisFrame)
 		{
-			PauseGame();
+			if (IsGamePaused)
+			{
+				ResumeGame();
+			}
+			else
+			{
+				PauseGame();
+			}
 		}
 	}
 }
